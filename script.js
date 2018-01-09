e9dabcd320144e572a7c2ca58473a0c8db666058
Larry Hosken <larry.hosken@token.io>
2018-01-08 17:25:48 -0800

    front-end += comments

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ Token.styleButton({            // Sets up the Link with Token button
             type: 'EMAIL',
             value: '{alias}'        // address filled in by server.js
         },
-        resources: [
-            { type: Token.RESOURCE_TYPE_ALL_ACCOUNTS },
-            { type: Token.RESOURCE_TYPE_ALL_BALANCES },
+        resources: [ // the button asks for permission to:
+            { type: Token.RESOURCE_TYPE_ALL_ACCOUNTS }, // get list of accounts
+            { type: Token.RESOURCE_TYPE_ALL_BALANCES }, // get balance of each account
         ]
     },
     function(data) { // success, have access token
         console.log('success callback got ' + JSON.stringify(data));
         if (data.tokenId) {
             tokenId = data.tokenId;
-            $.get(`/fetch-data`, {tokenId: tokenId}); // pass token id to server code
+            $.get(`/fetch-data`, {tokenId: tokenId}); // pass token id to server.js code
         }
     },
     function(error) { // fail
