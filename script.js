582f11e7db26add022068811b4c60f75d8190a89
grekinsky <greco.rubio@token.io>
2019-05-29 18:13:07 -0500

    Redirect as default, catch up with merchant sample

--- script.js
+++ script.js
@@ -2,11 +2,6 @@
 var tokenController;
 var button;
 
-// Client side Token object for creating the Token button, handling the popup, etc
-var token = new window.Token({
-    env: 'sandbox',
-});
-
 function clean() {
     if (button) {
         button.destroy();
@@ -19,30 +14,46 @@ function clean() {
     }
 }
 
-// set up a function using the item data to populate the request to fetch the TokenRequestFunction
-function getTokenRequestUrl(done) {
-    fetch('/request-balances', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-        },
-        body: JSON.stringify({resources: ['ACCOUNTS', 'BALANCES']}),
-    })
-    .then(function(response) {
-        if (response.ok) {
-            response.text()
-                .then(function(data) {
-                    // execute callback when successful response is received
-                    done(data);
-                    console.log('data: ', data);
-                });
-        }
-    });
-}
-
-function createButton(buttonType) {
+function createRedirectButton() {
     // clean up instances
     clean();
+
+    // Client side Token object for creating the Token button, handling the popup, etc
+    var token = new window.Token({
+        env: 'sandbox',
+    });
+
+    // get button placeholder element
+    var element = document.getElementById('tokenAccessBtn');
+
+    // create the button
+    button = token.createTokenButton(element, {
+        label: 'Token Access',
+    });
+
+    // create TokenController to handle messages
+    tokenController = token.createController();
+
+    // bind the Token Button to the Token Controller when ready
+    tokenController.bindButtonClick(
+        button, // Token Button
+        redirectTokenRequest, // redirect token request function
+        function(error) { // bindComplete callback
+            if (error) throw error;
+            // enable button after binding
+            button.enable();
+        },
+    );
+}
+
+function createPopupButton() {
+    // clean up instances
+    clean();
+
+    // Client side Token object for creating the Token button, handling the popup, etc
+    var token = new window.Token({
+        env: 'sandbox',
+    });
 
     // get button placeholder element
     var element = document.getElementById('tokenAccessBtn');
@@ -57,7 +68,7 @@ function createButton(buttonType) {
         onSuccess: function(data) { // Success Callback
             // ideally you should POST 'data' to your endpoint, but for simplicity's sake
             // we are simply putting it in the URL
-            var successURL = "/fetch-balances"
+            var successURL = "/fetch-balances-popup"
                 + "?data=" + window.encodeURIComponent(JSON.stringify(data));
             // navigate to success URL
             window.location.assign(successURL);
@@ -69,17 +80,42 @@ function createButton(buttonType) {
 
     // bind the Token Button to the Token Controller when ready
     tokenController.bindButtonClick(
-        button, // TokenButtonController
+        button, // Token Button
         getTokenRequestUrl, // token request function
         function(error) { // bindComplete callback
-            // enable button after binding
             if (error) throw error;
+            // enable button after binding
             button.enable();
         },
         { // options
-            desktop: buttonType,
+            desktop: 'POPUP',
         }
     );
+}
+
+function redirectTokenRequest() {
+    // go to request balances
+    document.location.assign("/request-balances");
+}
+
+// set up a function to fetch the Token Request Function
+function getTokenRequestUrl(done) {
+    fetch('/request-balances-popup', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+        },
+    })
+    .then(function(response) {
+        if (response.ok) {
+            response.text()
+                .then(function(data) {
+                    // execute callback when successful response is received
+                    done(data);
+                    console.log('data: ', data);
+                });
+        }
+    });
 }
 
 function setupButtonTypeSelector() {
@@ -93,10 +129,14 @@ function setupButtonTypeSelector() {
             var value = e.target.value;
             if (value === selected) return;
             selected = value;
-            createButton(value);
+            if (value === 'POPUP') {
+                createPopupButton();
+            } else if (value === 'REDIRECT') {
+                createRedirectButton();
+            }
         });
     }
-    createButton();
+    createRedirectButton();
 }
 
 setupButtonTypeSelector();
--- server.js
+++ server.js
@@ -11,37 +11,69 @@ var {TokenClient} = require('@token-io/tpp'); // main Token SDK entry object
 
 // 'sandbox': Connect to Sandbox testing environment
 // '4qY7...': Developer key
-var Token = new TokenClient({env: 'sandbox', developerKey: '4qY7lqQw8NOl9gng0ZHgT4xdiDqxqoGVutuZwrUYQsI'});
+var Token = new TokenClient({env: 'sandbox', developerKey: '4qY7lqQw8NOl9gng0ZHgT4xdiDqxqoGVutuZwrUYQsI', keyDir: './keys'});
 
-async function initServer() {
+async function init() {
+    var alias; // aisp alias
+    var member; // aisp member
+    // If we know of a previously-created aisp member, load it; else create a new one.
+
+    // Token SDK stores member keys in files in ./keys.
+    // If aisp member's ID is "m:1234:567", its key file is "m_1234_567".
+    var keyPaths;
+    try {
+        keyPaths = fs.readdirSync('./keys');
+    } catch (x) {
+        keyPaths = [];
+    }
+    if (keyPaths && keyPaths.length) {
+        var keyPath = keyPaths[0];
+        var mid = keyPath.replace(/_/g, ":");
+        member = Token.getMember(Token.UnsecuredFileCryptoEngine, mid);
+    }
+
+    // If member is defined, that means we found keys and loaded them.
+    if (member) {
+        // We're using an existing aisp member. Fetch its alias (email address)
+        try {
+            alias = await member.firstAlias();
+        } catch (e) {
+            console.log("Something went wrong: " + err);
+            console.log("If member ID not found or firstAlias fails, `rm -r ./keys` and try again.");
+            throw e;
+        }
+    } else {
+        // An alias is a human-readable way to identify a member, e.g., an email address or domain.
+        // When we tell Token UI to request an Access Token, we use this address.
+        // If a domain alias is used instead of an email, please contact Token
+        // with the domain and member ID for verification.
+        // See https://developer.token.io/sdk/#aliases for more information.
+        alias = {
+            type: 'EMAIL',
+            value: "asjs-" + Math.random().toString(36).substring(2, 10) + "+noverify@example.com"
+        };
+        member = await Token.createMember(alias, Token.UnsecuredFileCryptoEngine);
+        // A member's profile has a display name and picture.
+        // The Token UI shows this (and the alias) to the user when requesting access.
+        await member.setProfile({
+            // A member's profile has a display name and picture.
+            // The Token UI shows this (and the alias) to the user when requesting access.
+            displayNameFirst: 'Demo Data Aggregator',
+        });
+        await member.setProfilePicture('img/gif', 'R0lGODlhAQABAIABAP///wAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==');
+    }
+
+    // launch server
+    return initServer(member, alias);
+}
+
+async function initServer(member, alias) {
     app.use(cookieSession({
         name: 'session',
         keys: ['cookieSessionKey'],
         // Cookie Options
         maxAge: 24 * 60 * 60 * 1000, // 24 hours
     }));
-
-    // Create a Member (Token user account). A "real world" server would
-    // use the same member instead of creating a new one for each run;
-    // this demo creates a a new member for easier demos/testing.
-    var alias = {
-        // An alias is a human-readable way to identify a member, e.g., an email address or domain.
-        // When we tell Token UI to request an Access Token, we use this address.
-        // If a domain alias is used instead of an email, please contact Token
-        // with the domain and member ID for verification.
-        // See https://developer.token.io/sdk/#aliases for more information.
-        type: 'EMAIL',
-        value: 'asjs-' + Math.random().toString(36).substring(2, 10) + '+noverify@example.com'
-    };
-    var m = await Token.createMember(alias, Token.MemoryCryptoEngine);
-    await m.setProfile({
-        // A member's profile has a display name and picture.
-        // The Token UI shows this (and the alias) to the user when requesting access.
-        displayNameFirst: 'Demo Data Aggregator',
-    });
-    // empty placeholder image
-    await m.setProfilePicture('img/gif', 'R0lGODlhAQABAIABAP///wAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==');
-    var memberId = m.memberId();
 
     // Returns HTML file
     app.get('/', function (req, res) {
@@ -50,39 +82,51 @@ async function initServer() {
             res.send(contents);
         })
     });
-    // Returns client-side JS file
-    app.get('/script.js', function (req, res) {
-        fs.readFile('script.js', 'utf8', function (err, contents) {
-            res.send(contents);
-        })
-    });
 
-    app.post('/request-balances', urlencodedParser, async function (req, res) {
-        var member = Token.getMember(Token.MemoryCryptoEngine, memberId);
+    app.get('/request-balances', async function (req, res) {
+        var resources = ['ACCOUNTS', 'BALANCES'];
         var nonce = Token.Util.generateNonce();
         req.session.nonce = nonce;
-        var redirectUrl = req.protocol + '://' + req.get('host') + '/fetch-balances-redirect';
+        var redirectUrl = req.protocol + '://' + req.get('host') + '/fetch-balances';
 
         // set up the AccessTokenRequest
-        var tokenRequest = Token.createAccessTokenRequest(req.body.resources)
+        var tokenRequest = Token.createAccessTokenRequest(resources)
             .setToAlias(alias)
-            .setToMemberId(memberId)
+            .setToMemberId(member.memberId())
             .setRedirectUrl(redirectUrl)
             .setCSRFToken(nonce);
         // store the token request
-        member.storeTokenRequest(tokenRequest).then(function(request) {
-            var requestId = request.id;
-            var tokenRequestUrl = Token.generateTokenRequestUrl(requestId);
-            res.status(200).send(tokenRequestUrl);
-        });
+        var request = await member.storeTokenRequest(tokenRequest);
+        var requestId = request.id;
+        var tokenRequestUrl = Token.generateTokenRequestUrl(requestId);
+        res.redirect(302, tokenRequestUrl);
     });
 
-    // for popup flow, use Token.parseTokenRequestCallbackParams()
+    app.post('/request-balances-popup', urlencodedParser, async function (req, res) {
+        var resources = ['ACCOUNTS', 'BALANCES'];
+        var nonce = Token.Util.generateNonce();
+        req.session.nonce = nonce;
+        var redirectUrl = req.protocol + '://' + req.get('host') + '/fetch-balances-popup';
+
+        // set up the AccessTokenRequest
+        var tokenRequest = Token.createAccessTokenRequest(resources)
+            .setToAlias(alias)
+            .setToMemberId(member.memberId())
+            .setRedirectUrl(redirectUrl)
+            .setCSRFToken(nonce);
+        // store the token request
+        var request = await member.storeTokenRequest(tokenRequest);
+        var requestId = request.id;
+        var tokenRequestUrl = Token.generateTokenRequestUrl(requestId);
+        res.status(200).send(tokenRequestUrl);
+    });
+
+    // for redirect flow, use Token.parseTokenRequestCallbackUrl()
     app.get('/fetch-balances', async function (req, res) {
+        var callbackUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
         // verifies signature and CSRF token
-        var result = await Token.parseTokenRequestCallbackParams(JSON.parse(req.query.data), req.session.nonce);
+        var result = await Token.parseTokenRequestCallbackUrl(callbackUrl, req.session.nonce);
         // "log in" as service member.
-        var member = Token.getMember(Token.MemoryCryptoEngine, memberId);
         // get a Representable member object to use the access token
         var rep = member.forAccessToken(result.tokenId);
         var accounts = await rep.getAccounts();
@@ -96,13 +140,11 @@ async function initServer() {
         res.send(JSON.stringify(output)); // respond to script.js with balances
     });
 
-    // for redirect flow, use Token.parseTokenRequestCallbackUrl()
-    app.get('/fetch-balances-redirect', async function (req, res) {
-        var callbackUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    // for popup flow, use Token.parseTokenRequestCallbackParams()
+    app.get('/fetch-balances-popup', async function (req, res) {
         // verifies signature and CSRF token
-        var result = await Token.parseTokenRequestCallbackUrl(callbackUrl, req.session.nonce);
+        var result = await Token.parseTokenRequestCallbackParams(JSON.parse(req.query.data), req.session.nonce);
         // "log in" as service member.
-        var member = Token.getMember(Token.MemoryCryptoEngine, memberId);
         // get a Representable member object to use the access token
         var rep = member.forAccessToken(result.tokenId);
         var accounts = await rep.getAccounts();
@@ -122,4 +164,4 @@ async function initServer() {
     });
 }
 
-initServer();
+init();
