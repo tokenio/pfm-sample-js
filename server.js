59ad643ccb0e62d9ffefe13952ff67b93db3b159
Anna Nikulova <anna.nikulova@token.io>
2019-05-29 16:56:47 -0700

    Added picture to a profile and keys generation for a new member (like in merchant-demo-js)

--- server.js
+++ server.js
@@ -11,37 +11,17 @@ var {TokenClient} = require('@token-io/tpp'); // main Token SDK entry object
 
 // 'sandbox': Connect to Sandbox testing environment
 // '4qY7...': Developer key
-var Token = new TokenClient({env: 'sandbox', developerKey: '4qY7lqQw8NOl9gng0ZHgT4xdiDqxqoGVutuZwrUYQsI'});
+var Token = new TokenClient({env: 'sandbox', developerKey: '4qY7lqQw8NOl9gng0ZHgT4xdiDqxqoGVutuZwrUYQsI', keyDir: './keys'});
 
-async function initServer() {
+var member; // pfm member
+
+async function initServer(member, alias) {
     app.use(cookieSession({
         name: 'session',
         keys: ['cookieSessionKey'],
         // Cookie Options
         maxAge: 24 * 60 * 60 * 1000, // 24 hours
     }));
-
-    // Create a Member (Token user account). A "real world" server would
-    // use the same member instead of creating a new one for each run;
-    // this demo creates a a new member for easier demos/testing.
-    var alias = {
-        // An alias is a human-readable way to identify a member, e.g., an email address or domain.
-        // When we tell Token UI to request an Access Token, we use this address.
-        // If a domain alias is used instead of an email, please contact Token
-        // with the domain and member ID for verification.
-        // See https://developer.token.io/sdk/#aliases for more information.
-        type: 'EMAIL',
-        value: 'asjs-' + Math.random().toString(36).substring(2, 10) + '+noverify@example.com'
-    };
-    var m = await Token.createMember(alias, Token.MemoryCryptoEngine);
-    await m.setProfile({
-        // A member's profile has a display name and picture.
-        // The Token UI shows this (and the alias) to the user when requesting access.
-        displayNameFirst: 'Demo Data Aggregator',
-    });
-    // empty placeholder image
-    await m.setProfilePicture('img/gif', 'R0lGODlhAQABAIABAP///wAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==');
-    var memberId = m.memberId();
 
     // Returns HTML file
     app.get('/', function (req, res) {
@@ -58,14 +38,13 @@ async function initServer() {
     });
 
     app.post('/request-balances', urlencodedParser, async function (req, res) {
-        var member = Token.getMember(Token.MemoryCryptoEngine, memberId);
         var nonce = Token.Util.generateNonce();
         req.session.nonce = nonce;
 
         // set up the AccessTokenRequest
         var tokenRequest = Token.createAccessTokenRequest(req.body.resources)
             .setToAlias(alias)
-            .setToMemberId(memberId)
+            .setToMemberId(member.memberId())
             .setRedirectUrl('http://localhost:3000/fetch-balances-redirect')
             .setCSRFToken(nonce);
         // store the token request
@@ -80,8 +59,6 @@ async function initServer() {
     app.get('/fetch-balances', async function (req, res) {
         // verifies signature and CSRF token
         var result = await Token.parseTokenRequestCallbackParams(JSON.parse(req.query.data), req.session.nonce);
-        // "log in" as service member.
-        var member = Token.getMember(Token.MemoryCryptoEngine, memberId);
         // get a Representable member object to use the access token
         var rep = member.forAccessToken(result.tokenId);
         var accounts = await rep.getAccounts();
@@ -100,8 +77,6 @@ async function initServer() {
         var callbackUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
         // verifies signature and CSRF token
         var result = await Token.parseTokenRequestCallbackUrl(callbackUrl, req.session.nonce);
-        // "log in" as service member.
-        var member = Token.getMember(Token.MemoryCryptoEngine, memberId);
         // get a Representable member object to use the access token
         var rep = member.forAccessToken(result.tokenId);
         var accounts = await rep.getAccounts();
@@ -121,4 +96,54 @@ async function initServer() {
     });
 }
 
-initServer();
+// If we know of a previously-created merchant member, load it; else create a new one.
+
+// Token SDK stores member keys in files in ./keys.
+// If pfm member's ID is "m:1234:567", its key file is "m_1234_567".
+var keyPaths;
+try {
+    keyPaths = fs.readdirSync('./keys');
+} catch (x) {
+    keyPaths = [];
+}
+if (keyPaths && keyPaths.length) {
+    var keyPath = keyPaths[0];
+    var mid = keyPath.replace(/_/g, ":");
+    member = Token.getMember(Token.UnsecuredFileCryptoEngine, mid);
+}
+
+// If member is defined, that means we found keys and loaded them.
+if (member) {
+    // We're using an existing merchant member. Fetch its alias (email address)
+    member.firstAlias().then(function (alias) {
+        // launch server
+        initServer(member, alias);
+    }, function (err) {
+        console.log("Something went wrong: " + err);
+        console.log("If member ID not found or firstAlias fails, `rm -r ./keys` and try again.");
+    });
+} else {
+    var alias = {
+        // An alias is a human-readable way to identify a member, e.g., an email address or domain.
+        // When we tell Token UI to request an Access Token, we use this address.
+        // If a domain alias is used instead of an email, please contact Token
+        // with the domain and member ID for verification.
+        // See https://developer.token.io/sdk/#aliases for more information.
+        type: 'EMAIL',
+        value: 'asjs-' + Math.random().toString(36).substring(2, 10) + '+noverify@example.com'
+    };
+    Token.createMember(alias, Token.UnsecuredFileCryptoEngine).then(function (m) {
+        member = m;
+        // A member's profile has a display name and picture.
+        // The Token UI shows this (and the alias) to the user when requesting access.
+        member.setProfile({
+            displayNameFirst: 'Demo PFM'
+        })
+            .then(function () {
+                member.setProfilePicture('image/png', fs.readFileSync('southside.png'))
+            })
+            .then(function () {
+                initServer(member, alias)
+            });
+    });
+}
