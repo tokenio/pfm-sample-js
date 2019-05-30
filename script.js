aeb3c9cc6c8d173fe527dc393341eddb26b8e3f1
Greco Rubio <grekinsky@users.noreply.github.com>
2019-05-30 17:49:16 -0500

    Popup and redirect in the same button (#11)
    * Popup and redirect in the same button
    
    * Fix button text
    
    * Relative URLs
    
    * Redirect as default, catch up with merchant sample
    
    * Added picture to profile
    
    * Finvertex logo for PFM

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 "use strict";
-
-var elementId = "tokenAccessBtn";
 var tokenController;
 var button;
-
-// Client side Token object for creating the Token button, handling the popup, etc
-var Token = new window.Token({
-    env: 'sandbox',
-});
 
 function clean() {
     if (button) {
@@ -25,95 +18,104 @@ function createRedirectButton() {
     // clean up instances
     clean();
 
-    // create TokenPopupController to handle Popup messages
-    tokenController = Token.createRedirectController();
+    // Client side Token object for creating the Token button, handling the popup, etc
+    var token = new window.Token({
+        env: 'sandbox',
+    });
 
     // get button placeholder element
-    var element = document.getElementById(elementId);
+    var element = document.getElementById('tokenAccessBtn');
 
     // create the button
-    button = Token.createTokenButton(element, {
-        label: "Redirect Token Access",
+    button = token.createTokenButton(element, {
+        label: 'Token Access',
     });
 
-    // bind the Token Button to the Redirect Controller when ready
-    tokenController.bindButtonClick(button, function(action) {
-        // Each time the button is clicked, a new tokenRequestUrl is created
-        getTokenRequestUrl(function(tokenRequestUrl) {
-            // Initialize popup using the tokenRequestUrl
-            action(tokenRequestUrl);
-        });
-    });
-    // enable button after binding
-    button.enable();
+    // create TokenController to handle messages
+    tokenController = token.createController();
+
+    // bind the Token Button to the Token Controller when ready
+    tokenController.bindButtonClick(
+        button, // Token Button
+        redirectTokenRequest, // redirect token request function
+        function(error) { // bindComplete callback
+            if (error) throw error;
+            // enable button after binding
+            button.enable();
+        },
+    );
 }
 
 function createPopupButton() {
     // clean up instances
     clean();
 
-    // create TokenPopupController to handle Popup messages
-    tokenController = Token.createPopupController();
+    // Client side Token object for creating the Token button, handling the popup, etc
+    var token = new window.Token({
+        env: 'sandbox',
+    });
 
     // get button placeholder element
-    var element = document.getElementById(elementId);
+    var element = document.getElementById('tokenAccessBtn');
 
     // create the button
-    button = Token.createTokenButton(element, {
-        label: "Popup Token Access",
+    button = token.createTokenButton(element, {
+        label: 'Token Access',
     });
 
-    // setup onLoad callback
-    tokenController.onLoad(function(controller) {
-        // bind the Token Button to the Popup Controller when ready
-        tokenController.bindButtonClick(button, function(action) {
-            // Each time the button is clicked, a new tokenRequestUrl is created
-            getTokenRequestUrl(function(tokenRequestUrl) {
-                // Initialize popup using the tokenRequestUrl
-                action(tokenRequestUrl);
-            });
-        });
-        // enable button after binding
-        button.enable();
+    // create TokenController to handle messages
+    tokenController = token.createController({
+        onSuccess: function(data) { // Success Callback
+            // ideally you should POST 'data' to your endpoint, but for simplicity's sake
+            // we are simply putting it in the URL
+            var successURL = "/fetch-balances-popup"
+                + "?data=" + window.encodeURIComponent(JSON.stringify(data));
+            // navigate to success URL
+            window.location.assign(successURL);
+        },
+        onError: function(error) { // Failure Callback
+            throw error;
+        },
     });
 
-    // setup onSuccess callback
-    tokenController.onSuccess(function(data) { // Success Callback
-        // ideally you should POST 'data' to your endpoint, but for simplicity's sake
-        // we are simply putting it in the URL
-        var successURL = "/fetch-balances"
-            + "?data=" + window.encodeURIComponent(JSON.stringify(data));
-        // navigate to success URL
-        window.location.assign(successURL);
-    });
-
-    // setup onError callback
-    tokenController.onError(function(error) { // Failure Callback
-        throw error;
-    });
+    // bind the Token Button to the Token Controller when ready
+    tokenController.bindButtonClick(
+        button, // Token Button
+        getTokenRequestUrl, // token request function
+        function(error) { // bindComplete callback
+            if (error) throw error;
+            // enable button after binding
+            button.enable();
+        },
+        { // options
+            desktop: 'POPUP',
+        }
+    );
 }
 
+function redirectTokenRequest() {
+    // go to request balances
+    document.location.assign("/request-balances");
+}
+
+// set up a function to fetch the Token Request Function
 function getTokenRequestUrl(done) {
-    var XHR = new XMLHttpRequest();
-
-    //set up the access request
-    XHR.open("POST", "http://localhost:3000/request-balances", true);
-
-    XHR.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-
-    // resource types to request
-    var data = JSON.stringify({resources: ['ACCOUNTS', 'BALANCES']});
-
-    // Define what happens on successful data submission
-    XHR.addEventListener("load", function(event) {
-        // execute callback once response is received
-        if (event.target.status === 200) {
-            done(event.target.response);
+    fetch('/request-balances-popup', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+        },
+    })
+    .then(function(response) {
+        if (response.ok) {
+            response.text()
+                .then(function(data) {
+                    // execute callback when successful response is received
+                    done(data);
+                    console.log('data: ', data);
+                });
         }
     });
-
-    // Send the data; HTTP headers are set automatically
-    XHR.send(data);
 }
 
 function setupButtonTypeSelector() {
@@ -126,15 +128,15 @@ function setupButtonTypeSelector() {
         selector[i].addEventListener('click', function(e) {
             var value = e.target.value;
             if (value === selected) return;
-            if (value === 'popup') {
+            selected = value;
+            if (value === 'POPUP') {
                 createPopupButton();
-            } else if (value === 'redirect') {
+            } else if (value === 'REDIRECT') {
                 createRedirectButton();
             }
-            selected = value;
         });
     }
-    createPopupButton();
+    createRedirectButton();
 }
 
 setupButtonTypeSelector();
